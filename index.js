246e639dbd0622b91922b9d31215df22299b4d57
menhaj <menhaj007@yahoo.com>
2021-03-10 20:33:03 -0500

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(arr, birthday) {
+    const tmpArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        tmpArr[i] = `Thank you, ${arr[i]}, for the wonderful ${birthday} gift!`;
+    }
+    debugger;
+    return tmpArr;
+}
+
+function countDown(n) {
+    
+    while (n >= 0) {
+        console.log(n);
+        n--;
+        debugger;
+    }
+    
+}
